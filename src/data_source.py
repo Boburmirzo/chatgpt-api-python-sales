677ba8ce09d6fbe1f6be9f393e63f50a80eb5886
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2023-08-13 21:06:41 +0300

    Fix data source

--- src/data_source.py
+++ src/data_source.py
@@ -10,14 +10,11 @@ data_dir = os.environ.get("DATA_DIR", "./data/")
 
 class DataSourceType(Enum):
     CSV = "CSV"
-    RAINFOREST_API = "RAINFOREST_API"
 
 
 def connect(source_type, schema, params=None):
     if source_type == DataSourceType.CSV:
         return read_from_csv(data_dir, schema)
-    elif source_type == DataSourceType.RAINFOREST_API:
-        return read_from_rainforest_api(schema, params)
     else:
         raise ValueError(f"Unsupported data source type: {source_type}")
 
@@ -30,7 +27,3 @@ def read_from_csv(data_dir, schema):
         autocommit_duration_ms=50,
     )
     return sales_data
-
-
-def read_from_rainforest_api(schema, params):
-    return "Not yet implemented"
