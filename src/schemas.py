74dccf00747ad51f2468aca325ed0a399c0acabe
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2023-08-23 12:47:18 +0300

    Remove schema for generic CSV

--- src/schemas.py
+++ src/schemas.py
@@ -1,26 +1,6 @@
 import pathway as pw
 
 
-# Maps each data row into a structured document schema using Pathway
-class CsvDiscountsInputSchema(pw.Schema):
-    discount_until: str
-    country: str
-    city: str
-    state: str
-    postal_code: str
-    region: str
-    product_id: str
-    category: str
-    sub_category: str
-    brand: str
-    product_name: str
-    currency: str
-    actual_price: str
-    discount_price: str
-    discount_percentage: str
-    address: str
-
-
 class QueryInputSchema(pw.Schema):
     query: str
 
