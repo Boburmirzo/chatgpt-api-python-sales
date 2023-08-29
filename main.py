bdbe5facdc5417c595b613a4ea8271002b90a8dc
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2023-08-29 13:31:06 +0300

    Merge UI and refactor

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import importlib
 import os
 from dotenv import load_dotenv
+from crontab import CronTab
 
 load_dotenv()
 
-if __name__ == "__main__":
+cron = CronTab(user=True)
+job = cron.new(command='python3 examples/rainforest/data_ingestion_cron_job.py')
+job.minute.every(1)
+cron.write()
 
+if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", 8080))
 
-    mode = os.environ.get("APP_VARIANT", "csv.api")
-    app = importlib.import_module(f"examples.{mode}.app")
+    app = importlib.import_module("examples.app")
 
     app.run(host=host, port=port)
