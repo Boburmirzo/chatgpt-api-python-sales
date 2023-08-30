e891281eaf0385e2eebf3531284915ff9acba17a
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2023-08-30 14:02:26 +0300

    Move app.py to api

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", 8080))
 
-    app = importlib.import_module("examples.app")
+    app = importlib.import_module("examples.api.app")
 
     app.run(host=host, port=port)
